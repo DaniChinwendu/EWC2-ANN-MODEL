83444a3fe3108bea188f2464a77652afe37d2d33
DaniChinwendu <88294154+DaniChinwendu@users.noreply.github.com>
2023-02-19 15:44:52 +0100

    Update EWC-2.py

--- EWC-2.py
+++ EWC-2.py
@@ -6,7 +6,7 @@ import streamlit as st
 #collect the model and min-max object file
 filename ='EWC_2.pkl' 
 scaler='minmax_scaler_2.pkl'
-target='minmax_target(1).pkl'
+target='minmax_target1.pkl'
 
 def load_model():
     return pickle.load(open(filename,'rb'))
